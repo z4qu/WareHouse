f7ddbed94f5798a9a99d24ddc20d1936115d57f7
z4qu <59421804+z4qu@users.noreply.github.com>
2023-01-24 20:07:05 +0100

    Add Helpers

--- WareHouse/helpers.cs
+++ WareHouse/helpers.cs
@@ -2,5 +2,30 @@
 
 public class helpers
 {
+    public int GiveMeNumber()
+    {
+        
+        int number = 5;
+
+        return number;
+    }
+
+    public enum ItemTypes
+    {
+        Grocery,
+        Clothing,
+        Electronics
+    }
     
+    public struct SomeStructure
+    {
+        private int numberForStructure;
+        private string nameOfStructure;
+
+        public SomeStructure(int number, string name)
+        {
+            numberForStructure = number;
+            nameOfStructure = name;
+        }
+    }
 }
